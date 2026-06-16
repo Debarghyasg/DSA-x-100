b0dfe0ece89a520672676fccf37c8ce5f3dd0484
Debarghyasg <debarghya817@gmail.com>
2026-06-16 22:44:47 +0530

    common prefix,

---
+++ String/commonprefix.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class commonprefix1{
+    void commonprefix2(String s){
+        
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            for(int j=0;j<c.length();j++){
+
+            }
+
+        }
+
+    }
+}
+public class commonprefix{
+    public static void main(String[] args){
+        String s="apple,banana,watermelon";
+    }
+}
