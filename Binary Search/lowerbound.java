36759ebb2a9b527fd1f573b64daa0049c28c09d1
Debarghyasg <debarghya817@gmail.com>
2026-06-24 11:18:17 +0530

    Lower Bound in Binary Search

---
+++ Binary Search/lowerbound.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class lowerbound1{
+    int lowerbound2(int[] arr,int target){
+        int low=0;
+        int high=arr.length-1;
+        int mid=(low+high)/2;
+        while(low<high){
+        if(arr[mid]>=target){
+            return mid;
+        }
+        else if(arr[mid]<target){
+            low+=1;
+        }
+        else{
+            return arr.length;
+        }
+
+    }
+    return arr.length;
+}
+}
+
+
+public class lowerbound{
+    public static void main(String[] args){
+        int[] arr={1,2,2,3};
+        int target=2;
+        lowerbound1 obj=new lowerbound1();
+        System.out.println(obj.lowerbound2(arr,target));
+    }
+}
