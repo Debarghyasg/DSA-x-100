de90f6bcaffc1271c5046d8eb8be9fe838e4913a
Debarghyasg <debarghya817@gmail.com>
2026-08-11 11:01:24 +0530

    Missing number in arrays.

--- twosum.java
+++ twosum.java
@@ -1,5 +1,5 @@
 class twosum1{
-    int[] twosum2(int[] arr,int target){
+    public int[] twosum2(int[] arr,int target){
         int[] result=new int[2];
         for(int i=0;i<arr.length;i++){
             for(int j=1;j<arr.length;j++){
@@ -14,12 +14,4 @@ class twosum1{
     }
 }
 
-public class twosum {
-    public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
-        int target=7;
-        twosum1 obj=new twosum1();
-        obj.twosum2(arr,target);
 
-    }
-}
